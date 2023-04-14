bf3e835c26c8f76e97a4bd3878807a353c9aaf0f
Destiny Boone <destaj.db@gmail.com>
2023-04-14 16:09:46 -0400

    managing the token expiration

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,7 +1,26 @@
 import { redirect } from "react-router-dom";
 
+export function getTokenDuration(){
+    const storedExpirationDate = localStorage.getItem('expiration');
+    const expirationDate = new Date(storedExpirationDate);
+    const now = new Date();
+    const duration = expirationDate.getTime() - now.getTime();
+    return duration
+}
+
 export function getAuthToken(){
     const token = localStorage.getItem('token');
+
+    if(!token){
+        return null;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    if(tokenDuration < 0){ // aka if token expires
+        return 'EXPIRED'
+    }
+
     return token;
 }
 
